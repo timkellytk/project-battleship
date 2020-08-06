77025281222a85d762477537bf7cab706825a163
Tim Kelly <timpkelly1@gmail.com>
2020-08-06 12:37:08 +1000

    Updated the test and production code for Player.js

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,6 +1,19 @@
-export const attack = (col, row, gameboard) => {
-  const enemyGameboard = gameboard.receiveAttack(col, row, gameboard);
+import { gameboard } from '../Gameboard/Gameboard';
+
+export const attack = (col, row, attackGameboard) => {
+  const enemyGameboard = attackGameboard.receiveAttack(
+    col,
+    row,
+    attackGameboard
+  );
   return enemyGameboard;
 };
 
-export const player = () => {};
+export const player = () => {
+  const playerGameboard = () => gameboard();
+
+  return {
+    attack,
+    gameboard: playerGameboard,
+  };
+};
--- src/Components/Player/Player.test.js
+++ src/Components/Player/Player.test.js
@@ -3,25 +3,44 @@ import { gameboard } from '../Gameboard/Gameboard';
 import { shipFactory } from '../Ship/Ship';
 
 describe('attack()', () => {
-  test('player can attack enemy gameboard on empty cell', () => {
-    const initalGameboard = gameboard();
-    const testRow = { ...initalGameboard.board.A[1], hit: true };
-    const testCol = { ...initalGameboard.board.A, 1: testRow };
-    const testBoard = { ...initalGameboard.board, A: testCol };
-    const testGameboard = { ...initalGameboard, board: testBoard };
+  test('returns an updated board with {hit: true} on an empty cell', () => {
+    const initialGameboard = gameboard();
+    const testRow = { ...initialGameboard.board.A[1], hit: true };
+    const testCol = { ...initialGameboard.board.A, 1: testRow };
+    const testBoard = { ...initialGameboard.board, A: testCol };
+    const testGameboard = { ...initialGameboard, board: testBoard };
 
-    expect(attack('A', 1, initalGameboard)).toEqual(testGameboard);
+    expect(attack('A', 1, initialGameboard)).toEqual(testGameboard);
   });
 
-  test('player can attack enemy gameboard on cell with ship', () => {
-    const initalGameboard = gameboard();
+  test('returns an updated board with {hit: true} on an cell with a ship', () => {
+    let initialGameboard = gameboard();
     const initialShip = shipFactory(2);
-    initalGameboard.placeShip('B', 1, initialShip, initalGameboard);
-    const testRow = { ...initalGameboard.board.A[1], hit: true };
-    const testCol = { ...initalGameboard.board.A, 1: testRow };
-    const testBoard = { ...initalGameboard.board, A: testCol };
-    const testGameboard = { ...initalGameboard, board: testBoard };
+    initialGameboard = initialGameboard.placeShip(
+      'B',
+      1,
+      initialShip,
+      initialGameboard
+    );
+    const testRow = { ...initialGameboard.board.B[1], hit: true };
+    const testCol = { ...initialGameboard.board.B, 1: testRow };
+    const testBoard = { ...initialGameboard.board, B: testCol };
+    const testGameboard = { ...initialGameboard, board: testBoard };
 
-    expect(attack('A', 1, initalGameboard)).toEqual(testGameboard);
+    expect(attack('B', 1, initialGameboard)).toEqual(testGameboard);
+  });
+
+  test('returns an updated board with an extra hit on hitArray on the relevant ship', () => {
+    let initialGameboard = gameboard();
+    const testShip = initialGameboard.ships.carrier;
+    initialGameboard = initialGameboard.placeShip(
+      'C',
+      1,
+      testShip,
+      initialGameboard
+    );
+    expect(attack('C', 1, initialGameboard).ships.carrier.hitArray).toEqual([
+      'HIT',
+    ]);
   });
 });
