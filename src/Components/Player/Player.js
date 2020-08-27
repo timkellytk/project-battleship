28233d8137868ea90ea3fb0dc5490e7fdb70381c
Tim Kelly <timpkelly1@gmail.com>
2020-08-27 16:15:50 +1000

    Refactored the attack method into it's own function

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,23 +1,25 @@
 import gameboard from '../Gameboard/Gameboard';
 
+const attack = (col, row, enemy) => {
+  const updatedEnemyGameboard = enemy.gameboard.receiveAttack(col, row);
+
+  if (updatedEnemyGameboard === null) {
+    return null;
+  }
+
+  const updatedEnemy = {
+    ...enemy,
+    gameboard: updatedEnemyGameboard,
+  };
+  return updatedEnemy;
+};
+
 const player = () => {
   const playerGameboard = gameboard();
 
   return {
     gameboard: playerGameboard,
-    attack: (col, row, enemy) => {
-      const updatedEnemyGameboard = enemy.gameboard.receiveAttack(col, row);
-
-      if (updatedEnemyGameboard === null) {
-        return null;
-      }
-
-      const updatedEnemy = {
-        ...enemy,
-        gameboard: updatedEnemyGameboard,
-      };
-      return updatedEnemy;
-    },
+    attack,
   };
 };
 
