5d44db73b579e87c4de147d8c0fbc36f22102124
Tim Kelly <timpkelly1@gmail.com>
2020-08-27 17:27:54 +1000

    Created the Computer player and the relevant tests

--- src/Components/Player/Computer.js
+++ src/Components/Player/Computer.js
@@ -1,9 +1,32 @@
 import gameboard from '../Gameboard/Gameboard';
+import { rows, columns } from '../Constants/Constants';
+import { getRandomInt } from '../utils';
+
+const attack = (enemy) => {
+  const emptyCoordinates = [];
+
+  columns.forEach((col) => {
+    rows.forEach((row) => {
+      const emptyCell = !enemy.gameboard.board[col][row].hit;
+      if (emptyCell) {
+        emptyCoordinates.push({ col, row });
+      }
+    });
+  });
+
+  const randomCoordinateNumber = getRandomInt(0, emptyCoordinates.length - 1);
+  const { col, row } = emptyCoordinates[randomCoordinateNumber];
+
+  const updatedGameboard = enemy.gameboard.receiveAttack(col, row);
+  const updatedEnemy = { ...enemy, gameboard: updatedGameboard };
+  return updatedEnemy;
+};
 
 const computer = () => {
   const computerGameboard = gameboard();
   return {
     gameboard: computerGameboard,
+    attack,
   };
 };
 
---
+++ src/Components/utils.js
@@ -0,0 +1,7 @@
+// From Stack Overflow https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
