2682f00aeaa4c832c5b2a6ab7e966b3f091850d6
Tim Kelly <timpkelly1@gmail.com>
2020-09-01 13:46:54 +1000

    Installed Ant Design library and created the basic UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Button, Card, Space, Layout, Typography, Row, Col } from 'antd';
+import { PushpinTwoTone } from '@ant-design/icons';
+
+import './App.less';
+
+const { Title, Paragraph } = Typography;
+const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  return <div>hello world</div>;
+  const Header = () => (
+    <Row justify="center">
+      <Space direction="vertical" size="large" align="center">
+        <Title level={1}>Battleship</Title>
+        <Space>
+          <Button type="primary" size="large">
+            Play Game
+          </Button>
+          <Button type="default" size="large">
+            Randomise Ships
+          </Button>
+        </Space>
+      </Space>
+    </Row>
+  );
+  const Gameboards = () => (
+    <Row justify="center">
+      <Space size="large" align="center">
+        <Card style={{ height: '400px', width: '400px' }}>Player Board</Card>
+        <Card style={{ height: '400px', width: '400px' }}>Computer Board</Card>
+      </Space>
+    </Row>
+  );
+  const GameFooter = () => (
+    <Footer style={{ textAlign: 'center' }}>
+      <Paragraph>
+        Created by <a href="https://github.com/timkellytk">Tim Kelly</a> for The
+        Odin Project
+      </Paragraph>
+    </Footer>
+  );
+
+  return (
+    <Layout>
+      <div className="App">
+        <Space direction="vertical" size="large" align="center">
+          <Header />
+          <Gameboards />
+          <GameFooter />
+        </Space>
+      </div>
+    </Layout>
+  );
 };
 
 export default App;
