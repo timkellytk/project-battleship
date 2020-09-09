75961a7ede36503aff369d56479638f63dc49bab
Tim Kelly <timpkelly1@gmail.com>
2020-09-09 18:43:42 +1000

    Created the placeShip() method and tests

--- src/Game/Gameboard/Gameboard.js
+++ src/Game/Gameboard/Gameboard.js
@@ -3,6 +3,33 @@ import { BOARD_SIZE } from '../Constants/Constants';
 class Gameboard {
   constructor() {
     this.board = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill(''));
+    this.ships = [];
+  }
+  placeShip(ship) {
+    const coords = ship.getCoordinates();
+    const validCoordinates = coords.reduce((valid, { col, row }) => {
+      return col > 9 || col < 0 || row > 9 || row < 0 ? false : valid;
+    }, true);
+
+    const noShipExists = coords.reduce((noShip, coord) => {
+      const noExistingShip = this.ships.reduce((noOtherShip, otherShip) => {
+        const noExistingCoord = otherShip
+          .getCoordinates()
+          .reduce((noCoord, otherCoord) => {
+            const emptyCoord =
+              coord.col === otherCoord.col && coord.row === otherCoord.row;
+            return emptyCoord ? false : noCoord;
+          }, true);
+        return noExistingCoord ? noOtherShip : false;
+      }, true);
+      return noExistingShip ? noShip : false;
+    }, true);
+
+    if (validCoordinates && noShipExists) {
+      this.ships.push(ship);
+      return true;
+    }
+    return false;
   }
 }
 
--- src/Game/Gameboard/Gameboard.test.js
+++ src/Game/Gameboard/Gameboard.test.js
@@ -1,9 +1,11 @@
 /* 
 Test requirements for the Gameboard:
 - board = array(array(10 * null) * 10) (DONE)
-- ships = [create an array of all the ships]
+- ships = [] - set an empty array and allow yourself to intialiseShips with a function
 - placeShip(col, row, ship) = place ship at col, row coordinates
-- toggleShip(ship) = toggle the ship
+- initaliseShips = add all starting ships to the ship array
+- moveShip(col, row, ship)
+- toggleShip(ship) = toggle the ship's orientation
 - randomiseShips() = randomly place all the ships
 - receiveAttack(col, row) =>
   if(!attackShips(col, row)) {
@@ -13,6 +15,7 @@ Test requirements for the Gameboard:
 - gameover() = check if all ships are sunk
 */
 import Gameboard from './Gameboard';
+import Ship from '../Ship/Ship';
 
 describe('Gameboard', () => {
   let gameboard;
@@ -21,10 +24,30 @@ describe('Gameboard', () => {
   });
 
   test.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])(
-    'Create expected board',
+    'Create expected gameboard',
     (index) => {
       expect(gameboard.board.length).toEqual(10);
       expect(gameboard.board[index].length).toEqual(10);
+      expect(gameboard.ships).toEqual([]);
     }
   );
+  test.each([new Ship(0, 0, 2), new Ship(7, 5, 2), new Ship(6, 4, 3, false)])(
+    'placeShip() adds to ship array and returns true if ship has valid coords',
+    (placedShip) => {
+      expect(gameboard.placeShip(placedShip)).toEqual(true);
+      expect(gameboard.ships.length).toEqual(1);
+    }
+  );
+  test.each([new Ship(-1, 0, 2), new Ship(5, 9, 2), new Ship(9, 5, 2, false)])(
+    'placeShip() does not add to ship array and returns false if ship has invalid coords',
+    (placedShip) => {
+      expect(gameboard.placeShip(placedShip)).toEqual(false);
+      expect(gameboard.ships.length).toEqual(0);
+    }
+  );
+  test('placeShip() does not add to ship array and returns false if ship already exists at coords', () => {
+    gameboard.placeShip(new Ship(0, 0, 2));
+    expect(gameboard.placeShip(new Ship(0, 0, 2))).toEqual(false);
+    expect(gameboard.ships.length).toEqual(1);
+  });
 });
