aa50d722db64f893ddbb00332fd65d72983c3819
Tim Kelly <timpkelly1@gmail.com>
2020-09-16 18:14:45 +1000

    Updates

---
+++ precache-manifest.113ed843067765ca7aae721c2544dde0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1e7abe0925826efee7ea612c8f2f26",
+    "url": "/project-battleship/index.html"
+  },
+  {
+    "revision": "3eb660657191a49e7c5c",
+    "url": "/project-battleship/static/css/main.704f05e9.chunk.css"
+  },
+  {
+    "revision": "188d5b39a15441f6c3d2",
+    "url": "/project-battleship/static/js/2.da1f7def.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/project-battleship/static/js/2.da1f7def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb660657191a49e7c5c",
+    "url": "/project-battleship/static/js/main.58e4addb.chunk.js"
+  },
+  {
+    "revision": "edde4f0743caa6db60ae",
+    "url": "/project-battleship/static/js/runtime-main.b0a7044e.js"
+  }
+]);
