3e812053c9f96b51460b24601148bdc4bf02b0dd
Tim Kelly <timpkelly1@gmail.com>
2020-09-17 14:55:46 +1000

    Refactored Computer.js

--- src/Game/Player/Computer.js
+++ src/Game/Player/Computer.js
@@ -18,55 +18,54 @@ const getRandomCoordinate = (gameboard) => {
     },
     []
   );
-  const emptyIndex = getRandomInt(0, emptyCoordinates.length - 1);
-  const { row, col } = emptyCoordinates[emptyIndex];
+  const randomIndex = getRandomInt(0, emptyCoordinates.length - 1);
+  const { row, col } = emptyCoordinates[randomIndex];
   return { row, col };
 };
 
-const getEmptySurroundingCoordinate = (shipExistsAtCoords, gameboard) => {
-  for (let i = 0; i < shipExistsAtCoords.length; i++) {
-    const surroundingCoords = getSurroundingCoords(shipExistsAtCoords[i]);
-    const emptySurroundingCoords = surroundingCoords.filter(({ row, col }) => {
-      return (
-        validCoordinate(row, col) &&
-        gameboard[row][col] !== 'MISS' &&
-        gameboard[row][col] !== 'HIT'
-      );
-    });
-    const anyCellAvailable = surroundingCoords.some(({ row, col }) => {
-      return (
-        validCoordinate(row, col) &&
-        gameboard[row][col] !== 'MISS' &&
-        gameboard[row][col] !== 'HIT'
-      );
-    });
+const emptyCoordinate = (row, col, gameboard) =>
+  gameboard[row][col] !== 'MISS' && gameboard[row][col] !== 'HIT';
 
+const getSurroundingEmptyCoordinate = (hitShipArray, gameboard) => {
+  for (let i = 0; i < hitShipArray.length; i++) {
+    const surroundingCoords = getSurroundingCoords(hitShipArray[i]);
+    const anyCellAvailable = surroundingCoords.some(
+      ({ row, col }) =>
+        validCoordinate(row, col) && emptyCoordinate(row, col, gameboard)
+    );
     if (anyCellAvailable) {
-      const emptyIndex = getRandomInt(0, emptySurroundingCoords.length - 1);
-      const { row, col } = emptySurroundingCoords[emptyIndex];
-      return { row, col };
+      const emptySurroundingCoords = surroundingCoords.filter(
+        ({ row, col }) =>
+          validCoordinate(row, col) && emptyCoordinate(row, col, gameboard)
+      );
+      const randomIndex = getRandomInt(0, emptySurroundingCoords.length - 1);
+      const { row, col } = emptySurroundingCoords[randomIndex];
+      return {
+        row,
+        col,
+      };
     }
   }
 };
 
 const getComputerCoordinate = (gameboard) => {
-  if (shipExistsAtCoords.length > 0) {
-    return getEmptySurroundingCoordinate(shipExistsAtCoords, gameboard.board);
+  if (hitShipArray.length > 0) {
+    return getSurroundingEmptyCoordinate(hitShipArray, gameboard.board);
   }
   return getRandomCoordinate(gameboard);
 };
 
-let shipExistsAtCoords = [];
+let hitShipArray = [];
 
 class Computer extends Player {
   attack(enemy) {
     const { row, col } = getComputerCoordinate(enemy.gameboard);
     const result = enemy.gameboard.receiveAttack(row, col);
     if (result === 'HIT') {
-      shipExistsAtCoords.push({ row, col });
+      hitShipArray.push({ row, col });
     }
     if (result === 'SUNK') {
-      shipExistsAtCoords = [];
+      hitShipArray = [];
     }
     return result;
   }
